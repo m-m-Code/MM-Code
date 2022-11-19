97be755bba77fd38c6527daac042d04735281d87
hussein bahdon <35459369+HusseinMccoin@users.noreply.github.com>
2022-11-19 03:06:46 +0000

    updated activity page

--- activity.js
+++ activity.js
@@ -1,11 +1,19 @@
 let editor = document.querySelector("#editor");
 
 ace.edit(editor, {
-  theme: "ace/theme/tomorrow1",
+  theme: "ace/theme/twilight",
   mode: "ace/mode/html",
 });
 
-
+var js = ace.createEditSession("some js code");
 var css = ace.createEditSession(["some", "css", "code here"]);
+// and then to load document into editor, just call
+editor.setSession(js);
 
-ediotr.setSession(css);
+var showCode = document.querySelector("code p").innerText("hello");
+
+
+//
+// var noCopy= = document.querySelector(".coding-exercise-workplace");
+//
+//   noCopy.onpaste = returnfalse;
