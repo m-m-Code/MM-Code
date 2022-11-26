84d0a8e54ef33d47c718fbe49c4cd58151b3578d
hussein bahdon <35459369+HusseinMccoin@users.noreply.github.com>
2022-11-26 05:16:18 +0000

    updated the activity page

--- activity.js
+++ activity.js
@@ -1,61 +1,75 @@
+var doneButton = document.querySelector('.doneButton');
+var runCode = document.querySelector('.runcode');
+var blockCode = document.querySelector('textarea');
+var checkButton1 = document.querySelector('.checkButton');
+var infobox = document.querySelector('.infobox');
 
-  var doneButton = document.querySelector('.doneButton');
-  var runCode = document.querySelector('.runcode');
-  var blockCode = document.querySelector('textarea');
-  var checkButton1 = document.querySelector('.checkButton');
-  var infobox = document.querySelector('.infobox');
+
+// Setup Ace
+let codeEditor = ace.edit("editor");
+
+let defaultCode =
+'<h1> Hello World </h1>'
+'<p> hello </p>';
 
 
-  // Setup Ace
-  let codeEditor = ace.edit("editor");
+let userCode = ' ';
 
-  //codeEditor.addEventListener("copy", stop, true)
+//let defaultCode2 = '<p> this is a paragraph </p>'
 
-  let defaultCode = '<h1> Hello World </h1>';
-  let defaultCode3 = '<p> hello </p>'
+let editorLib = {
+  init() {
+    // Configure Ace
 
-  let userCode = ' ';
+    // Theme
+    codeEditor.setTheme("ace/theme/clouds_midnight");
 
-  //let defaultCode2 = '<p> this is a paragraph </p>'
+    // Set language
+    codeEditor.session.setMode("ace/mode/html");
 
-  let editorLib = {
-    init() {
-      // Configure Ace
+    // Set Options
+    codeEditor.setOptions({
+      highlightSelectedWord: false,
+      fontSize: '12pt',
+      newLineMode: "auto",
+      highlightGutterLine: true,
+      selectionStyle: 'line',
 
-      // Theme
-      codeEditor.setTheme("ace/theme/xcode");
 
-      // Set language
-      codeEditor.session.setMode("ace/mode/html");
+    });
+    // Set Default Code
+    codeEditor.setValue(defaultCode);
+    codeEditor.clearSelection();
+    // codeEditor.firstLineNumber(defaultCode3);
 
-      // Set Options
-      codeEditor.setOptions({
-
-      });
-
-      // Set Default Code
-      codeEditor.setValue(defaultCode);
-      codeEditor.setValue(defaultCode3);
-      codeEditor.onpaste ="return false";
-    }
   }
+}
+
+function noCheating() {
+  codeEditor.onCopy = function() {
+    alert("This is cheating, focus on writing it out")
+  };
+  codeEditor.onCut = function() {
+    alert("This is cheating, focus on writing it out")
+  };
+  codeEditor.onPaste = function() {
+    alert("This is cheating, focus on writing it out")
+  };
+  codeEditor.getCopyText = function() {
+    alert("This is cheating, focus on writing it out")
+  };
+
+}
+
+// doneButton.addEventListener('click', () => {
+//   // Clear ace editor
+//   codeEditor.setValue(' ');
+//   doneButton.style.display='none';
+//   checkButton1.hidden= false;
+//   infobox.hidden=true;
+// })
 
 
-
-  checkButton1.addEventListener('click', ()=>{
-
-  })
-
-
-
-  doneButton.addEventListener('click', () => {
-    // Clear ace editor
-    codeEditor.setValue(' ');
-    doneButton.style.display='none';
-    checkButton1.hidden= false;
-    infobox.hidden=true;
-  })
-
-
-  /// this is where the code runs
-  editorLib.init();
+/// this is where the code runs
+editorLib.init();
+noCheating();
