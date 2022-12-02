aa1b87c1fe2e6e404edc3bba5593f8b780dcba63
hussein bahdon <35459369+HusseinMccoin@users.noreply.github.com>
2022-12-02 00:41:19 +0000

    updated the files

--- activity.js
+++ activity.js
@@ -1,29 +1,18 @@
     // Setup Ace
     let codeEditor = ace.edit("editor");
     let exercise1 = '<h1>Hello World</h1>';
-    // let exercise2 = '<p> hello </p>';
-    //
-    // let exercise3 = '<p> bye </p>';
-    // let exercise4 = '<p> morning </p>';
-
     let check = document.querySelector(".nextExercise");
-
-    let userCode = ' ';
+    // let userCode = ' ';
 
     var e = '<ul>\n <li>Coffee</li> \n <li>Tea</li> \n <li>Milk</li>\n</ul>'
-
-    //let defaultCode2 = '<p> this is a paragraph </p>'
 
     let editorLib = {
       init() {
         // Configure Ace
-
         // Theme
         codeEditor.setTheme("ace/theme/dracula");
-
         // Set language
         codeEditor.session.setMode("ace/mode/html");
-
         // Set Options
         codeEditor.setOptions({
           highlightSelectedWord: false,
@@ -31,41 +20,22 @@
           newLineMode: "auto",
           highlightGutterLine: true,
           selectionStyle: 'line',
-
-
         });
         // Set Default Code
         codeEditor.setValue(currentExcise);
         //currentExcise
         nextExercise();
         muscleMemory();
-
-
         // codeEditor.firstLineNumber(defaultCode3);
 
       }
     }
-
-    // '<h1>This is heading 1</h1>',
-    // '<p>This is a paragraph.</p>',
-    // '<a href="https://www.w3schools.com">This is a link</a>',
-    // '<img src="w3schools.jpg" alt="W3Schools.com" width="104" height="142">',
-    // '<button>Click me</button>',
-    // '<ul>\n <li>Coffee</li> \n <li>Tea</li> \n <li>Milk</li>\n</ul>',
-
-    var exercises = [
-      '<h1> hello </h1>',
-      '<p> bye </p>',
-      '<button> button </button>',
-    ]
 
     function ChooseRandomExcise(arr) {
       RandomExcise = Math.floor(Math.random() * 3);
       var item = arr[RandomExcise];
       return item
     }
-
-
 
     //this function get ridden of the checkButton and display the done doneButton
     //this function chooses a random exercise from the array we created
@@ -94,16 +64,12 @@
       });
     };
 
-
-
     function muscleMemory() {
       var memory = 1;
       for (var i = 0; i < memory; i++) {
         memory[checkUserCode()];
       }
-
     }
-
 
     // no cheating  function
     function noCheating() {
@@ -122,10 +88,8 @@
 
     }
 
-
     var doneButtonValue = document.querySelector('.doneButton');
     var checkButtonValue = document.querySelector(".checkButton");
-
     function beginExcises() {
       doneButtonValue.addEventListener('click', () => {
         codeEditor.setValue('');
@@ -133,8 +97,6 @@
         checkButtonValue.hidden = false;
       })
     }
-
-
     /// this is where the code runs
     editorLib.init();
     noCheating();
---
+++ html/html-exercises.js
@@ -0,0 +1,8 @@
+  var exercises = [
+    '<h1>This is heading 1</h1>',
+    '<p>This is a paragraph.</p>',
+    '<a href="#">This is a link</a>',
+    '<img src="#.jpg" alt="#.com" width="104" height="142">',
+    '<button>Click me</button>',
+    '<ul>\n <li>Coffee</li> \n <li>Tea</li> \n <li>Milk</li>\n</ul>',
+  ]
