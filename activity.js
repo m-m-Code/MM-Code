5f71d2910c2abea8a2fd0db94551670cadd2b995
hussein bahdon <35459369+HusseinMccoin@users.noreply.github.com>
2022-12-13 15:44:32 +0000

    updated files

--- activity.js
+++ activity.js
@@ -3,6 +3,8 @@
     let exercise1 = '<h1>Hello World</h1>';
     let check = document.querySelector(".nextExercise");
     // let userCode = ' ';
+    let currentExercise = ChooseRandomExercise(exercisesHtml);
+    let userCode = codeEditor.getValue();
 
     var e = '<ul>\n <li>Coffee</li> \n <li>Tea</li> \n <li>Milk</li>\n</ul>'
 
@@ -22,91 +24,36 @@
           selectionStyle: 'line',
         });
         // Set Default Code
-        codeEditor.setValue(currentExcise);
-        //currentExcise
-        nextExercise();
-        muscleMemory();
+        codeEditor.setValue(currentExercise);
         // codeEditor.firstLineNumber(defaultCode3);
 
       }
     }
 
-    let url = document.createElement('a');
-    url
-
-    function switchExericse(){
-      if (url.href = '') {
-
-      }
-    }
-
-    function ChooseRandomExcise(arr) {
-      RandomExcise = Math.floor(Math.random() * 3);
-      var item = arr[RandomExcise];
+    function ChooseRandomExercise(arr) {
+      RandomExercise = Math.floor(Math.random() * exercisesHtml.length);
+      var item = arr[RandomExercise];
       return item
     }
 
-    //this function get ridden of the checkButton and display the done doneButton
     //this function chooses a random exercise from the array we created
     function nextExercise() {
       document.querySelector(".nextExercise").addEventListener("click", function() {
-        codeEditor.setValue(ChooseRandomExcise(exercises));
-        doneButtonValue.style.display = 'block';
-        checkButtonValue.hidden = true;
+        codeEditor.setValue(ChooseRandomExercise(exercisesHtml));
       });
-    }
-
-    //this var gets the currentExcise
-    var currentExcise = ChooseRandomExcise(exercises);
-
-    //check the users code only
-    function checkUserCode() {
-      document.querySelector(".checkButton").addEventListener("click", function() {
-        var userCode = codeEditor.getValue();
-        if (userCode === currentExcise) {
-          console.log(currentExcise);
-          alert("well done G");
-          codeEditor.setValue('');
-        } else {
-          alert("its wrong fam")
-        };
-      });
-    };
-
-    function muscleMemory() {
-      var memory = 1;
-      for (var i = 0; i < memory; i++) {
-        memory[checkUserCode()];
-      }
     }
 
     // no cheating  function
     function noCheating() {
-      codeEditor.onCopy = function() {
-        alert("This is cheating, focus on writing it out")
-      };
-      codeEditor.onCut = function() {
-        alert("This is cheating, focus on writing it out")
-      };
-      codeEditor.onPaste = function() {
-        alert("This is cheating, focus on writing it out")
-      };
-      codeEditor.getCopyText = function() {
-        alert("This is cheating, focus on writing it out")
-      };
-
+      codeEditor.onCopy = function(){}
+      codeEditor.onCut = function(){}
+      codeEditor.onPaste = function(){}
+      codeEditor.getCopyText = function(){}
     }
 
-    var doneButtonValue = document.querySelector('.doneButton');
-    var checkButtonValue = document.querySelector(".checkButton");
-    function beginExcises() {
-      doneButtonValue.addEventListener('click', () => {
-        codeEditor.setValue('');
-        doneButtonValue.style.display = 'none';
-        checkButtonValue.hidden = false;
-      })
-    }
+  
+
     /// this is where the code runs
     editorLib.init();
     noCheating();
-    beginExcises();
+    beginExercises();
--- htmlExercise/html-exercises.js
+++ htmlExercise/html-exercises.js
@@ -1,8 +1,12 @@
-  var exercises = [
+  var exercisesHtml = [
     '<h1>This is heading 1</h1>',
     '<p>This is a paragraph.</p>',
     '<a href="#">This is a link</a>',
     '<img src="#.jpg" alt="#.com" width="104" height="142">',
     '<button>Click me</button>',
     '<ul>\n <li>Coffee</li> \n <li>Tea</li> \n <li>Milk</li>\n</ul>',
+    '<h1> Ebu </h1>',
+    '<h1> huss </h1>',
+    '<h1> huss2 </h1>',
+    '<h1> Ebu2 </h1>',
   ]
